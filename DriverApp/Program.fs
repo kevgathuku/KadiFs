26b552b6ef0c103ba1386cf51a4414d11a19c5c4
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-10-23 20:54:16 +0200

    Move top level app to Project solution
    Add separate console driver app
    Only the solution file in the top level

---
+++ DriverApp/Program.fs
@@ -0,0 +1,9 @@
+﻿open KadiFs.Poker
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Starting game..."
+    Game.runStateMachine Game.initialState
--- Program.fs
+++
@@ -1,7 +0,0 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
-
-[<EntryPoint>]
-let main argv =
-    printfn "Starting game..."
-    Poker.runStateMachine Poker.initialState
