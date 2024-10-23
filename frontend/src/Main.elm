68e9e8bb76e0944579dbed7b9d3ad74753697596
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-10-23 22:20:20 +0200

    Add elm frontend app

---
+++ frontend/src/Main.elm
@@ -0,0 +1,57 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+type alias Model =
+    { count : Int }
+
+
+initialModel : Model
+initialModel =
+    { count = 0 }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( { model | count = model.count + 1 }, Cmd.none )
+
+        Decrement ->
+            ( { model | count = model.count - 1 }, Cmd.none )
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Elm app"
+    , body =
+        [ div []
+            [ button [ onClick Increment ] [ text "+1" ]
+            , div [] [ text <| String.fromInt model.count ]
+            , button [ onClick Decrement ] [ text "-1" ]
+            ]
+        ]
+    }
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = \_ -> init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
